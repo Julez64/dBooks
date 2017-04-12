5f8ad227a945ff5209a7bd9ec7daad1d3b887527
JulienFerluc <ferluc.julien@carrefour.cegepvicto.ca>
2017-04-12 09:09:38 -0400

    Setup Gruntfile to compile sass and minify it

---
+++ gruntfile.js
@@ -0,0 +1,43 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {
+                    compass: true,
+                    style: 'expanded'
+                },
+                files: {
+                    'css/styles.css' : 'sass/styles.scss'
+                }
+            }
+        },
+
+        cssmin: {
+            dist: {
+                files: {
+                    'css/styles.min.css' : 'css/styles.css'
+                }
+            }
+        },
+
+        watch: {
+            sass: {
+                files: ['sass/**/*.scss'],
+                tasks: ['sass']
+            },
+            styles: {
+                files: ['css/styles.css'],
+                tasks: ['cssmin']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['sass', 'cssmin']);
+};
