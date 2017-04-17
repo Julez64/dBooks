354af1730c07a18eed27ff07fb01a17b37417d95
JulienFerluc <ferluc.julien@carrefour.cegepvicto.ca>
2017-04-16 21:54:40 -0400

    Smaller books

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -13,7 +13,7 @@ var books = {
         url: "https://about.canva.com/wp-content/uploads/sites/3/2015/01/business_bookcover.png"
     },
     three: {
-        title: "Moby Dick",
+        title: "Moby-Dick",
         author: "Herman Melville",
         url: "http://i.dailymail.co.uk/i/pix/2011/06/15/article-2003632-0C90802A00000578-22_233x351.jpg"
     },
@@ -26,6 +26,16 @@ var books = {
         title: "Sherlock Holmes",
         author: "Sir Arthur Conan Doyle",
         url: "http://www.aboutgreatbooks.com/wp-content/uploads/2015/09/the-complete-sherlock-holmes-sherlock-holmes-books-679x1024.jpg"
+    },
+    six: {
+        title: "Cagliostro",
+        author: "Vicente Huidobro",
+        url: "https://s-media-cache-ak0.pinimg.com/564x/dc/c7/d0/dcc7d0deee6cde61c04a42bba33fe749.jpg"
+    },
+    seven: {
+        title: "Les aventure de Tintin: Le Trésor de Rackham le Rouge",
+        author: "Hérgé",
+        url: "https://images-na.ssl-images-amazon.com/images/I/A1JjDSaitGL.jpg"
     }
 };
 
