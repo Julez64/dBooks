c559463b05d6f4e3e3c028ea72d47fc5ac7d8910
JulienFerluc <ferluc.julien@carrefour.cegepvicto.ca>
2017-04-18 22:52:05 -0400

    Added Mysql

--- routes/livre.js
+++ routes/livre.js
@@ -1,12 +1,37 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 /* GET home page. */
 router.get('/livre/:id', function(req, res, next) {
-    res.render('livre', {
-        title: "dBooks",
-        id: req.params.id
+
+    var connection = mysql.createConnection({
+        host: "192.168.2.65",
+        user: "readonly",
+        password: "uQgbI7IKaP1gn91n",
+        database: "dBooks"
+    });
+
+    connection.connect(connection);
+
+    connection.query('SELECT * FROM `Livres` WHERE `ID` = ?', [req.params.id], (err, result, fields) => {
+        if(err || !result){
+            res.header("404");
+            throw err;
+        } else {
+            console.log("Query Successful!" + result[0].TitreLivre);
+        }
+
+        res.render('livre', {
+            title: "dBooks",
+            TitreLivre: result[0].TitreLivre,
+            Annee: result[0].AnneeParution
+        });
+
+        connection.end();
+        console.log("Connection Ended!");
     });
 });
 
+    
 module.exports = router;
